9aec5c101853c7d6f75db6d09f9ee699aeb3f3d9
BSONGERISTS <135406685+BSONGERISTS@users.noreply.github.com>
2024-07-18 13:18:02 +0800

    posh
    almost

--- PublishGuide.js
+++ PublishGuide.js
@@ -2,6 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript loaded and ready');
 });
 
+var GuideBtn = document.getElementById("guide-nav-btn");
+// 
+var GDBtn = GuideBtn.getElementsByClassName("guide-nav-style");
+// 
+for (var i = 0; i < GDBtn.length; i++) {
+        GDBtn[i].addEventListener("click", function() {
+            var current = document.getElementsByClassName("gnstyle-active");
+            current[0].className = current[0].className.replace(" gnstyle-active", "");
+            this.className += " gnstyle-active";
+        } 
+    );
+}
 
 // Get the head element and all elements with the class 'popup'
 var head = document.getElementById("head");
@@ -18,5 +30,16 @@ function togglePopup() {
     pop.classList.toggle("login-index");
 }
 
+    var extend1 = document.getElementById("body1");
+    var extend2 = document.getElementById("body2");
 
+function Body1() {
+    extend1.classList.add("body-wrap-e"),
+    extend2.classList.remove("body-wrap-e2");
+}
 
+function Body2() {
+    extend2.classList.add("body-wrap-e2"),
+    extend1.classList.remove("body-wrap-e");
+    
+}
