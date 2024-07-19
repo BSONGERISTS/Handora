407f1a676a37a33ca756cec839cbcc2cc4ca8c45
Raven Balbastro <balbastroraven@gmail.com>
2024-07-19 12:36:59 +0800

    profileScript from main added

--- ProfileScript.js
+++ ProfileScript.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript loaded');
+
     fetch('session_status.php')
         .then(response => response.json())
         .then(data => {
@@ -13,52 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         })
         .catch(error => console.error('Error fetching session status:', error));
-
-
-    var header = document.getElementById("butts");
-    // 
-    var btns = header.getElementsByClassName("btn-style");
-    // 
-    for (var i = 0; i < btns.length; i++) {
-            btns[i].addEventListener("click", function() {
-                var current = document.getElementsByClassName("active");
-                current[0].className = current[0].className.replace(" active", "");
-                this.className += " active";
-            } 
-        );
-    }
-
-    function guide(){
-        var element = document.getElementById("1s");
-        element.classList.add("position")
-
-        var element2 = document.getElementById("2s");
-        element2.classList.remove("position")
-    }
-
-    function diss(){
-        var element3 = document.getElementById("1s");
-        element3.classList.remove("position")
-
-        var element4 = document.getElementById("2s");
-        element4.classList.add("position")
-    }
-
-    function PopOpen(){
-        var PopElementOpen = document.getElementById("pop");
-        PopElementOpen.classList.remove("pop-index")
-    }
-
-    function PopClose(){
-        var PopElementClose = document.getElementById("pop");
-        PopElementClose.classList.add("pop-index")
-    }
-
-    function Prof1(){
-        var Prof1 = document.getElementById("user-prof");
-        Prof1.src = "./Assets/MCLogo.svg";
-    }
-
 
     const updateProfile = document.getElementById("updateProfile");
     console.log (updateProfile);
@@ -80,59 +35,58 @@ document.addEventListener('DOMContentLoaded', () => {
             })
     })
 
-
-    var header = document.getElementById("butts");
-    
-    var btns = header.getElementsByClassName("btn-style");
-    
-    for (var i = 0; i < btns.length; i++) {
-            btns[i].addEventListener("click", function() {
-                var current = document.getElementsByClassName("active");
-                current[0].className = current[0].className.replace(" active", "");
-                this.className += " active";
-            } 
-        );
-    }
-
-    function guide(){
-        var element = document.getElementById("1s");
-        element.classList.add("position")
-
-        var element2 = document.getElementById("2s");
-        element2.classList.remove("position")
-    }
-
-    function diss(){
-        var element3 = document.getElementById("1s");
-        element3.classList.remove("position")
-
-        var element4 = document.getElementById("2s");
-        element4.classList.add("position")
-    }
-
-    function PopOpen(){
-        var PopElementOpen = document.getElementById("pop");
-        PopElementOpen.classList.remove("pop-index")
-    }
-
-    function PopClose(){
-        var PopElementClose = document.getElementById("pop");
-        PopElementClose.classList.add("pop-index")
-    }
-
-    function PopOpen2(){
-        var PopElementOpen2 = document.getElementById("pop2");
-        PopElementOpen2.classList.remove("pop-index2")
-    }
-
-    function PopClose2(){
-        var PopElementClose2 = document.getElementById("pop2");
-        PopElementClose2.classList.add("pop-index2")
-    }
-
-    function Prof1(){
-        var Prof1 = document.getElementById("user-prof");
-        Prof1.src = "./Assets/MCLogo.svg";
-    }
-
 });
+
+var header = document.getElementById("butts");
+// 
+var btns = header.getElementsByClassName("btn-style");
+// 
+for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        } 
+    );
+}
+
+function guide(){
+    var element = document.getElementById("1s");
+    element.classList.add("position")
+
+    var element2 = document.getElementById("2s");
+    element2.classList.remove("position")
+}
+
+function diss(){
+    var element3 = document.getElementById("1s");
+    element3.classList.remove("position")
+
+    var element4 = document.getElementById("2s");
+    element4.classList.add("position")
+}
+
+function PopOpen(){
+    var PopElementOpen = document.getElementById("pop");
+    PopElementOpen.classList.remove("pop-index")
+}
+
+function PopClose(){
+    var PopElementClose = document.getElementById("pop");
+    PopElementClose.classList.add("pop-index")
+}
+
+function PopOpen2(){
+    var PopElementOpen2 = document.getElementById("pop2");
+    PopElementOpen2.classList.remove("pop-index2")
+}
+
+function PopClose2(){
+    var PopElementClose2 = document.getElementById("pop2");
+    PopElementClose2.classList.add("pop-index2")
+}
+
+function Prof1(){
+    var Prof1 = document.getElementById("user-prof");
+    Prof1.src = "./Assets/MCLogo.svg";
+}
