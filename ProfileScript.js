2eec61fe5963eb61e411659b3553c8edb477f61c
BSONGERISTS <135406685+BSONGERISTS@users.noreply.github.com>
2024-07-19 12:32:58 +0800

    adsasd
    javvascrip

--- ProfileScript.js
+++ ProfileScript.js
@@ -1,56 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript loaded');
-    var header = document.getElementById("butts");
-    // 
-    var btns = header.getElementsByClassName("btn-style");
-    // 
-    for (var i = 0; i < btns.length; i++) {
-            btns[i].addEventListener("click", function() {
-                var current = document.getElementsByClassName("active");
-                current[0].className = current[0].className.replace(" active", "");
-                this.className += " active";
-            } 
-        );
-    }
-    
-    function guide(){
-        var element = document.getElementById("1s");
-        element.classList.add("position")
-    
-        var element2 = document.getElementById("2s");
-        element2.classList.remove("position")
-    }
-    
-    function diss(){
-        var element3 = document.getElementById("1s");
-        element3.classList.remove("position")
-    
-        var element4 = document.getElementById("2s");
-        element4.classList.add("position")
-    }
-    
-    function PopOpen(){
-        var PopElementOpen = document.getElementById("pop");
-        PopElementOpen.classList.remove("pop-index")
-    }
-    
-    function PopClose(){
-        var PopElementClose = document.getElementById("pop");
-        PopElementClose.classList.add("pop-index")
-    }
-    
-    function PopOpen2(){
-        var PopElementOpen2 = document.getElementById("pop2");
-        PopElementOpen2.classList.remove("pop-index2")
-    }
-    
-    function PopClose2(){
-        var PopElementClose2 = document.getElementById("pop2");
-        PopElementClose2.classList.add("pop-index2")
-    }
-    
-    function Prof1(){
-        var Prof1 = document.getElementById("user-prof");
-        Prof1.src = "./Assets/MCLogo.svg";
-    }
 });
+
+var header = document.getElementById("butts");
+// 
+var btns = header.getElementsByClassName("btn-style");
+// 
+for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        } 
+    );
+}
+
+function guide(){
+    var element = document.getElementById("1s");
+    element.classList.add("position")
+
+    var element2 = document.getElementById("2s");
+    element2.classList.remove("position")
+}
+
+function diss(){
+    var element3 = document.getElementById("1s");
+    element3.classList.remove("position")
+
+    var element4 = document.getElementById("2s");
+    element4.classList.add("position")
+}
+
+function PopOpen(){
+    var PopElementOpen = document.getElementById("pop");
+    PopElementOpen.classList.remove("pop-index")
+}
+
+function PopClose(){
+    var PopElementClose = document.getElementById("pop");
+    PopElementClose.classList.add("pop-index")
+}
+
+function PopOpen2(){
+    var PopElementOpen2 = document.getElementById("pop2");
+    PopElementOpen2.classList.remove("pop-index2")
+}
+
+function PopClose2(){
+    var PopElementClose2 = document.getElementById("pop2");
+    PopElementClose2.classList.add("pop-index2")
+}
+
+function Prof1(){
+    var Prof1 = document.getElementById("user-prof");
+    Prof1.src = "./Assets/MCLogo.svg";
+}
