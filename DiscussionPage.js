de0ff92369fcda2af46b46b0055bf130c1a270e4
BSONGERISTS <135406685+BSONGERISTS@users.noreply.github.com>
2024-07-22 01:14:15 +0800

    new
    discussion page

---
+++ DiscussionPage.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript loaded and ready');
+});
+
+
+// Get the head element and all elements with the class 'popup'
+var head = document.getElementById("head");
+var btns = head.getElementsByClassName("popup");
+
+// Add event listeners to all popup buttons
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", togglePopup);
+}
+
+// Function to toggle the pop-up visibility
+function togglePopup() {
+    var pop = document.getElementById("pop");
+    pop.classList.toggle("login-index");
+}
