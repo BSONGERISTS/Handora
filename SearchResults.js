bd81cbf26cceec2430d3617c907500ed9f8e5940
BSONGERISTS <135406685+BSONGERISTS@users.noreply.github.com>
2024-07-29 11:13:39 +0800

    updattt
    js poof

--- SearchResults.js
+++ SearchResults.js
@@ -1,30 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('JavaScript loaded and ready');
-});
 
-
-// Get the head element and all elements with the class 'popup'
-var head = document.getElementById("head");
-var btns = head.getElementsByClassName("popup");
-
-// Add event listeners to all popup buttons
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", togglePopup);
-}
-
-// Function to toggle the pop-up visibility
-function togglePopup() {
-    var pop = document.getElementById("pop");
-    pop.classList.toggle("login-index");
-}
 
 // Search nav \\
 var header = document.getElementById("snav");
 // 
-var btns = header.getElementsByClassName("search-btn");
+var btans = header.getElementsByClassName("search-btn");
 // 
-for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function() {
+for (var i = 0; i < btans.length; i++) {
+        btans[i].addEventListener("click", function() {
             var current = document.getElementsByClassName("sbtn-active");
             current[0].className = current[0].className.replace(" sbtn-active", "");
             this.className += " sbtn-active";
