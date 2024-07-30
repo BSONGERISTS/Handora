0625f2c49523b2b6441381a6c83634a4ec0ff038
Raven Balbastro <balbastroraven@gmail.com>
2024-07-30 09:01:47 +0800

    bug fix

--- PublishGuide.js
+++ PublishGuide.js
@@ -15,14 +15,7 @@ for (var i = 0; i < GDBtn.length; i++) {
     );
 }
 
-// Get the head element and all elements with the class 'popup'
-var head = document.getElementById("head");
-var btns = head.getElementsByClassName("popup");
 
-// Add event listeners to all popup buttons
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", togglePopup);
-}
 
 // Function to toggle the pop-up visibility
 function togglePopup() {
